0a1105a028017dfaf9a50adbcde8f5ef9e978e99
ppmagdum12 <66131817+ppmagdum12@users.noreply.github.com>
2020-08-28 15:34:55 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+let x = document.querySelector("#login");
+let y = document.querySelector("#register");
+let z = document.querySelector("#btn");
+
+function admin() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+function user() {
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+}
